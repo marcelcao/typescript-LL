d116b6b26994336156b5a5f64650a5eb1a20687a
Mara Caoile <mara.caoile@gmail.com>
2024-04-02 21:37:34 -0500

    wrote notes on the challenge solution

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -40,10 +40,15 @@ function addTodoItem(todo: string): Todo {
 
     return newTodo
 }
+//for this function, we defined the type for todo which is a string
+//we also want to reuse the Todo interface when we return newTodo with the new values//
 
 function getNextId<T extends {id: number }>(items: T[]): number {
     return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
 }
+
+//for this function we turned it into a generic type by defining the type that items will be which will be an array and the value which will always be a number//
+//howerver, we needed to extend the generic type because the code doesn't know what id is, so we had to define its primitive value as well//
 
 const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
 
